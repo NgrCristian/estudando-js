5eba11498567def5b853f95ec3d2804827c4b06a
NgrCristian <cristian.barreto.ne@gmail.com>
2023-07-11 10:22:30 -0300

    funcoes

---
+++ aula 100723/funcoes.js
@@ -0,0 +1,69 @@
+// const num = 3;
+// const num2 = 6;
+// const soma = num + num2;
+// console.log(soma);
+
+// let n = 16;
+// let quadrado = n**2;  // ou n*n
+
+// let num1 = 4;
+// let num2 = 5;
+// function multiplicar(){
+//     console.log("O numero 1 é:" + num1);
+//     console.log("O numero 2 é:" + num2);
+//     console.log("O produto é " + (num1 * num2));
+// }
+// multiplicar()
+
+
+// console.log("HELLO");
+
+// isNaN(num); // retorna true or false
+
+// isFinite(num); // retorna true or false
+
+// parseInt(num); // conversao do float para int
+
+// parseFloat(num); //conversao de int para float
+
+// result = Number.isInteger(); // retorna se e inteiro (true/false)
+
+// numero.toFixed(2) // quantidade de casas decimais
+
+// numero.toPrecision(4)
+
+// numero.toString() // transforma em string
+
+
+// const texto = "JavaScript";
+// console.log(texto.charAt(0));
+
+// console.log(texto.concat("bata", "ta"));
+
+// console.log(texto.trim()); // remove espaço na frente e no final do texto
+
+// console.log(numero.replace("." , ","));
+
+// console.log(texto.includes("Java")); // usado para busca!!!
+
+// if( texto.includes(Java)) {
+//     console.log("Java ou JavaScript");
+// }
+
+// console.log(texto.toLowerCase());
+// console.log(texto.toUpperCase());
+
+// console.log(texto.toLowerCase(). includes("Java"));
+
+// let soma = 0;
+// for(let i = 1; i <= 100; soma += i++){
+//     console.log(soma)
+// }
+
+
+console.log(Math.ceil(2.389)); // arredonda para cima
+console.log(Math.floor(2.389)); //arredonda para baixo
+console.log(Math.round(2.389)); // arredonda para o mais proximo
+console.log(Math.sqrt(121)); // raiz quadrada
+console.log(Math.abs(-8)); // modulo
+console.log(Math.floor(Math.random() *100 +1));
